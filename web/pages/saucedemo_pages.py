4c3189931f8d452e39503283aa59c22ec79b53e3
kaielly Sousa <kaiellysousa8530@gmail.com>
2026-05-05 22:54:33 -0300

    ajuste final selenium

--- web/pages/saucedemo_pages.py
+++ web/pages/saucedemo_pages.py
@@ -22,5 +22,8 @@ class SaucedemoPage:
         self.driver.find_element(By.ID, "last-name").send_keys(sobrenome)
         self.driver.find_element(By.ID, "postal-code").send_keys(cep)
         
-        self.driver.find_element(By.ID, "continue").click()
-        self.driver.find_element(By.ID, "finish").click()
+        btn_continue = self.driver.find_element(By.ID, "continue")
+        self.driver.execute_script("arguments[0].click();", btn_continue)
+        
+        btn_finish = self.driver.find_element(By.ID, "finish")
+        self.driver.execute_script("arguments[0].click();", btn_finish)
